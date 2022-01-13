a89a6a7a5056c619ebafb0e495db2b90293965e6
Mike Wagz <mikehwagz@users.noreply.github.com>
2022-01-13 17:27:57 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,96 @@
+module.exports = {
+  theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      current: 'currentColor',
+      transparent: 'transparent',
+    },
+    fontFamily: {
+      sans: ['sans-serif'],
+      serif: ['serif'],
+    },
+    screens: {
+      xs: '400px',
+      s: '600px',
+      m: '800px',
+      l: '1000px',
+      xl: '1200px',
+      '2xl': '1400px',
+      '3xl': '1600px',
+      ...new Array(13)
+        .fill()
+        .map((_, i) => i * 100 + 400)
+        .reduce((acc, val) => {
+          acc[val] = `${val}px`
+          return acc
+        }, {}),
+    },
+    fontSize: new Array(201)
+      .fill()
+      .map((_, i) => i)
+      .reduce((acc, val) => {
+        acc[val] = `${val / 10}rem`
+        return acc
+      }, {}),
+    lineHeight: new Array(161)
+      .fill()
+      .map((_, i) => i)
+      .reduce((acc, val) => {
+        acc[val] = `${val / 100}`
+        return acc
+      }, {}),
+    spacing: new Array(351)
+      .fill()
+      .map((_, i) => i)
+      .reduce((acc, val) => {
+        acc[val] = `${val / 10}rem`
+        return acc
+      }, {}),
+    opacity: new Array(21)
+      .fill()
+      .map((_, i) => i * 5)
+      .reduce((acc, val) => {
+        acc[val] = `${val / 100}`
+        return acc
+      }, {}),
+    zIndex: new Array(11)
+      .fill()
+      .map((_, i) => i)
+      .reduce((acc, val) => {
+        acc[val] = `${val}`
+        return acc
+      }, {}),
+    transitionTimingFunction: {
+      DEFAULT: 'cubic-bezier(0.23, 1, 0.32, 1)',
+      'in-quad': 'cubic-bezier(0.55, 0.085, 0.68, 0.53)',
+      'in-cubic': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)',
+      'in-quart': 'cubic-bezier(0.895, 0.03, 0.685, 0.22)',
+      'in-quint': 'cubic-bezier(0.755, 0.05, 0.855, 0.06)',
+      'in-sine': 'cubic-bezier(0.47, 0, 0.745, 0.715)',
+      'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+      'in-circ': 'cubic-bezier(0.6, 0.04, 0.98, 0.335)',
+      'in-back': 'cubic-bezier(0.6, -0.28, 0.735, 0.045)',
+      'out-quad': 'cubic-bezier(0.25, 0.46, 0.45, 0.94)',
+      'out-cubic': 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+      'out-quart': 'cubic-bezier(0.165, 0.84, 0.44, 1)',
+      'out-quint': 'cubic-bezier(0.23, 1, 0.32, 1)',
+      'out-sine': 'cubic-bezier(0.39, 0.575, 0.565, 1)',
+      'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+      'out-circ': 'cubic-bezier(0.075, 0.82, 0.165, 1)',
+      'out-back': 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+      'in-out-quad': 'cubic-bezier(0.455, 0.03, 0.515, 0.955)',
+      'in-out-cubic': 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+      'in-out-quart': 'cubic-bezier(0.77, 0, 0.175, 1)',
+      'in-out-quint': 'cubic-bezier(0.86, 0, 0.07, 1)',
+      'in-out-sine': 'cubic-bezier(0.445, 0.05, 0.55, 0.95)',
+      'in-out-expo': 'cubic-bezier(1, 0, 0, 1)',
+      'in-out-circ': 'cubic-bezier(0.785, 0.135, 0.15, 0.86)',
+      'in-out-back': 'cubic-bezier(0.68, -0.55, 0.265, 1.55)',
+    },
+    stroke: (theme) => theme('colors'),
+    fill: (theme) => theme('colors'),
+    extend: {},
+  },
+  plugins: [],
+}
